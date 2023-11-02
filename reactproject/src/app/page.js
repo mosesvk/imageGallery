02fa617c67bf951a6c117931eb8cc5d52980e0fc
Andrew Rasband <andrew.rasband4492@stu.mtec.edu>
2023-11-02 10:20:52 -0600

    hopefully dont break this has tailwind congfigured

---
+++ reactproject/src/app/page.js
@@ -0,0 +1,9 @@
+// import Image from 'next/image'
+import './globals.css'
+export default function Home() {
+  return (
+    <div className='bg-black'>
+    <h1 className='text-white'>Hello</h1>
+    </div>
+  )
+}
