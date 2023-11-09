26abd5cb11ffd6db479c6c0804d287f1a6e8c7cc
Andrew Rasband <andrew.rasband@outlook.com>
2023-11-09 09:38:32 -0700

    is this the head

--- reactproject/next.config.js
+++ reactproject/next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.pexels.com',
-                port: '',
-                pathname: '/photos/18971850/pexels-photo-18971850.jpeg'
-            }
-        ]
-    }
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/photos/18971850/pexels-photo-18971850.jpeg",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
 /*Error: Invalid src prop (https://images.pexels.com/photos/18971850/pexels-photo-18971850.jpeg?auto=compress&cs=tinysrgb&h=350) on `next/image`, hostname "images.pexels.com" is not configured under images in your `next.config.js`
 See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host*/
 
