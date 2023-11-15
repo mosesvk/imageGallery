454b385d23b3e5020ab545fdf0245aef86217493
Andrew Rasband <andrew.rasband@outlook.com>
2023-11-15 08:51:52 -0700

    deleted old react website. had problems with babel, eslint, material UI, and app router. no we are using pages router and no babel or eslint.

--- react-version-2/tailwind.config.js
+++ react-version-2/tailwind.config.js
@@ -2,9 +2,6 @@
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/Main/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/searchEngine/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/api/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
